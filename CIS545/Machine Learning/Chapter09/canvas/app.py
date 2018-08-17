61e3e5279025df4c75b971f89436fb9c64f1d745
Shawn <tsuihsiangyang@gmail.com>
2018-08-17 11:35:15 -0400

    Machine Learning

---
+++ CIS545/Machine Learning/Chapter09/canvas/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import base64
+from scipy import misc
+# import imageio
+import io
+import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('canvas.html')
+  
+@app.route('/canvas', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      data_url = request.headers.get('PHOTO')
+      content = data_url.split(';')[1]
+      image_encoded = content.split(',')[1]
+      # body = base64.decodebytes(image_encoded.encode('utf-8'))
+      body = base64.b64decode(image_encoded)
+      # image4d = imageio.imread(io.BytesIO(body))
+      image4d = misc.imread(io.BytesIO(body))
+      image = image4d[:,:,3]
+      image = -(image - 255)
+      misc.imsave('./static/imageToSave.png', image)
+
+      return "Success"
+   if request.method == 'GET':
+      return "Response"
+    
+if __name__ == '__main__':
+   app.run(debug = True)
